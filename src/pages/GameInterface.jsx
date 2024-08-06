2892a27e2ec5b12aab69de721d1e79c0607e0e92
Scott Fitzgerald <john.fitzgerald@epitech.eu>
2024-08-06 19:15:19 +0200

    implement user choice

--- src/pages/GameInterface.jsx
+++ src/pages/GameInterface.jsx
@@ -1,7 +1,38 @@
+import { useState, useEffect } from "react";
+
 function GameInterface() {
+
+    const [userChoice, setUserChoice] = useState("");
+
+    const [buttonsDisabled, setButtonsDisabled] = useState(false);
+
+
+    const handleClick = (choice) => {
+        setUserChoice(choice);
+        setButtonsDisabled(true);
+
+    }
+
+
+    useEffect(() => {
+        console.log(userChoice);
+    }, [userChoice]);
+
     return (
         <>
         <h1>Hello World</h1>
+
+        <div className="options">
+            <div className={`option ${buttonsDisabled ? "disabled" : ""}`} onClick={() => !buttonsDisabled && handleClick('Rock')}>
+                <p>Rock</p>
+            </div>
+            <div className={`option ${buttonsDisabled ? "disabled" : ""}`} onClick={() => !buttonsDisabled && handleClick('Paper')}>
+                <p>Paper</p>
+            </div>
+            <div className={`option ${buttonsDisabled ? "disabled" : ""}`} onClick={() => !buttonsDisabled && handleClick('Scissors')}>
+                <p>Scissors</p>
+            </div>
+        </div>
         </>
     );
 }
